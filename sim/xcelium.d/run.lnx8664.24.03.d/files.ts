5411ff285f05d21966d1e64642e9c5813a5b4a6e
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-09-23 17:41:44 -0300

    Modificando Teste

--- sim/xcelium.d/run.lnx8664.24.03.d/files.ts
+++ sim/xcelium.d/run.lnx8664.24.03.d/files.ts
@@ -2,7 +2,7 @@
 1758652519 /home/xmen/Documents/ShiftMatrix/rtl/ShiftMatrix.sv
 1758643684 /home/xmen/Documents/ShiftMatrix/rtl/celula_base.sv
 1758652510 /home/xmen/Documents/ShiftMatrix/rtl/top_mult_celula.sv
-1758652654 /home/xmen/SystolicMatrixMultiplier-/tb/tb.sv
+1758659777 /home/xmen/SystolicMatrixMultiplier-/tb/tb.sv
 1758654105 /home/xmen/SystolicMatrixMultiplier-/rtl/ShiftMatrix.sv
 1758643684 /home/xmen/SystolicMatrixMultiplier-/rtl/celula_base.sv
 1758654252 /home/xmen/SystolicMatrixMultiplier-/rtl/top_mult_celula.sv
