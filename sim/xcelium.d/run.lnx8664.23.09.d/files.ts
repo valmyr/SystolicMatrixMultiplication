fad247e61f85b4252e569f651706ec5838246d2a
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-09-24 01:01:42 -0300

    Removendo diretório de simulação

--- sim/xcelium.d/run.lnx8664.23.09.d/files.ts
+++
@@ -1,4 +0,0 @@
-1758679359 /home/valmir.silva/SystolicMatrixMultiplier/tb/tb.sv
-1758678954 /home/valmir.silva/SystolicMatrixMultiplier/rtl/ShiftMatrix.sv
-1758679417 /home/valmir.silva/SystolicMatrixMultiplier/rtl/accumulator.sv
-1758679473 /home/valmir.silva/SystolicMatrixMultiplier/rtl/systolicMatrixMultiply.sv
