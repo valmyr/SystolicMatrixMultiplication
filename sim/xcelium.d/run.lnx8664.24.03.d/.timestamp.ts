01403d6e77d466e5d3c23490db9f62b1fb91fc59
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-09-25 16:56:26 -0300

    Test 100x100

--- sim/xcelium.d/run.lnx8664.24.03.d/.timestamp.ts
+++ sim/xcelium.d/run.lnx8664.24.03.d/.timestamp.ts
@@ -1,4 +1,4 @@
 1758806274 /home/xmen/SystolicMatrixMultiplier/rtl/ShiftMatrix.sv
-1758825359 /home/xmen/SystolicMatrixMultiplier/tb/tb.sv
+1758829859 /home/xmen/SystolicMatrixMultiplier/tb/tb.sv
 1758822397 /home/xmen/SystolicMatrixMultiplier/rtl/systolicMatrixMultiply.sv
 1758805774 /home/xmen/SystolicMatrixMultiplier/rtl/accumulator.sv
