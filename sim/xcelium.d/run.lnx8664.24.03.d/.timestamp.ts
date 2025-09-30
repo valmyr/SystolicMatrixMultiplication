82eb1832ecf5260b74b80443a5d69dbd8485982b
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-09-30 00:34:23 -0300

    Ajuste de tb

--- sim/xcelium.d/run.lnx8664.24.03.d/.timestamp.ts
+++ sim/xcelium.d/run.lnx8664.24.03.d/.timestamp.ts
@@ -1,8 +1,4 @@
-1759190712 /home/xmen/Desktop/SystolicMatrixMultiplication/tb/tb.sv
-1758830888 /home/xmen/SystolicMatrixMultiplier/rtl/ShiftMatrix.sv
+1759203194 /home/xmen/Desktop/SystolicMatrixMultiplication/tb/tb.sv
 1759181853 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/accumulator.sv
-1758833306 /home/xmen/SystolicMatrixMultiplier/tb/tb.sv
-1759185742 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/ShiftMatrix.sv
+1759190779 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/ShiftMatrix.sv
 1759189256 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/systolicMatrixMultiply.sv
-1758822397 /home/xmen/SystolicMatrixMultiplier/rtl/systolicMatrixMultiply.sv
-1758805774 /home/xmen/SystolicMatrixMultiplier/rtl/accumulator.sv
--- sim/xcelium.d/run.lnx8664.24.03.d/files.ts
+++ sim/xcelium.d/run.lnx8664.24.03.d/files.ts
@@ -1,8 +1,4 @@
-1758919097 /home/xmen/SystolicMatrixMultiplier/tb/tb.sv
-1758830888 /home/xmen/SystolicMatrixMultiplier/rtl/ShiftMatrix.sv
-1758805774 /home/xmen/SystolicMatrixMultiplier/rtl/accumulator.sv
-1758914172 /home/xmen/SystolicMatrixMultiplier/rtl/systolicMatrixMultiply.sv
-1759190712 /home/xmen/Desktop/SystolicMatrixMultiplication/tb/tb.sv
-1759185742 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/ShiftMatrix.sv
+1759203194 /home/xmen/Desktop/SystolicMatrixMultiplication/tb/tb.sv
+1759190779 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/ShiftMatrix.sv
 1759181853 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/accumulator.sv
 1759189256 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/systolicMatrixMultiply.sv
