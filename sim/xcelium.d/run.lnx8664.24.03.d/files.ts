65c7c186dbbfa82b8f055a52d935ed7cf6ed0c8b
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-10-02 17:25:24 -0300

    linhas serializadas

--- sim/xcelium.d/run.lnx8664.24.03.d/files.ts
+++
@@ -1,4 +0,0 @@
-1759419608 /home/xmen/Desktop/SystolicMatrixMultiplication/tb/tb.sv
-1759419420 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/ShiftMatrix.sv
-1759371008 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/accumulator.sv
-1759413368 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/systolicMatrixMultiply.sv
