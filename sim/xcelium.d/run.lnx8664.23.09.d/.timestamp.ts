9f8588b73f685e5ddc914346c7d2955853105ea9
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-10-24 10:37:10 -0300

    Ok

--- sim/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
+++ sim/xcelium.d/run.lnx8664.23.09.d/.timestamp.ts
@@ -1,4 +1,4 @@
-1761308311 /home/valmir.silva/Desktop/validacao/analise/SystolicMatrixMultiplication/rtl/systolicMatrixMultiply.sv
+1761312764 /home/valmir.silva/Desktop/validacao/analise/SystolicMatrixMultiplication/rtl/systolicMatrixMultiply.sv
 1761265318 /home/valmir.silva/Desktop/validacao/analise/SystolicMatrixMultiplication/rtl/accumulator.sv
-1761312360 /home/valmir.silva/Desktop/validacao/analise/SystolicMatrixMultiplication/tb/tb.sv
+1761312953 /home/valmir.silva/Desktop/validacao/analise/SystolicMatrixMultiplication/tb/tb.sv
 1761311013 /home/valmir.silva/Desktop/validacao/analise/SystolicMatrixMultiplication/rtl/ShiftMatrix.sv
