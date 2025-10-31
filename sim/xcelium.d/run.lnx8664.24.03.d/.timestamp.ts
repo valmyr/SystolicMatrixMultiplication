db8fbd354c3e4712e9dec3138051e9c2e778f9fc
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-10-30 22:47:41 -0300

    ok

--- sim/xcelium.d/run.lnx8664.24.03.d/.timestamp.ts
+++ sim/xcelium.d/run.lnx8664.24.03.d/.timestamp.ts
@@ -1,4 +1,8 @@
+1761836330 /home/xmen/Desktop/compara/analise/rtl/accumulator.sv
+1761842623 /home/xmen/Desktop/compara/analise/tb/tb.sv
 1761835308 /home/xmen/Desktop/SystolicMatrixMultiplication/tb/tb.sv
+1761836330 /home/xmen/Desktop/compara/analise/rtl/ShiftMatrix.sv
+1761836330 /home/xmen/Desktop/compara/analise/rtl/systolicMatrixMultiply.sv
 1761706691 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/accumulator.sv
 1761834167 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/ShiftMatrix.sv
 1761706815 /home/xmen/Desktop/SystolicMatrixMultiplication/rtl/systolicMatrixMultiply.sv
