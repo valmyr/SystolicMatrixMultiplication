952fbb1ef3d449ed131b98492b65c7ce5ec96060
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-12-02 16:08:50 -0300

    melhorias no janelamento: passos de 3x

--- SystolicCore/SystolicCore.runs/impl_1/Arty7_top_sim.tcl
+++ SystolicCore/SystolicCore.runs/impl_1/Arty7_top_sim.tcl
@@ -97,8 +97,6 @@ proc step_failed { step } {
 OPTRACE "impl_1" END { }
 }
 
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 
 OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Init Design" START { ROLLUP_AUTO }
@@ -106,12 +104,10 @@ start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
-  set_param checkpoint.writeSynthRtdsInDcp 1
   set_param chipscope.maxJobs 8
   set_param xicom.use_bs_reader 1
   set_param tcl.collectionResultDisplayLimit 0
   set_param general.usePosixSpawnForFork 1
-  set_param synth.incrementalSynthesisCache ./.Xil/Vivado-560498-VT0144/incrSyn
   set_param runs.launchOptions { -jobs 32  }
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7a100tcsg324-1
