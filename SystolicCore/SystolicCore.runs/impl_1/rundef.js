dd7cbbd68526e76ecaa5e006832a348ad040465e
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-12-10 02:07:41 -0300

    parametrizaç~ao do janelamento

--- SystolicCore/SystolicCore.runs/impl_1/rundef.js
+++ SystolicCore/SystolicCore.runs/impl_1/rundef.js
@@ -28,7 +28,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log Arty7_top_sim.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source Arty7_top_sim.tcl -notrace" );
 
--- SystolicCore/SystolicCore.runs/impl_1/runme.sh
+++ SystolicCore/SystolicCore.runs/impl_1/runme.sh
@@ -38,7 +38,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log Arty7_top_sim.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source Arty7_top_sim.tcl -notrace
 
 
